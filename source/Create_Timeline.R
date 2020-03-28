8974265a410f542ac4d687501f20bd55b6e218a6
Sarah Leavitt <sv1205@bu.edu>
2020-03-28 15:18:08 -0400

    Updated table and figure code

--- source/Create_Timeline.R
+++ source/Create_Timeline.R
@@ -6,31 +6,41 @@ library(tidyr)
 library(ggplot2)
 
 ## Reading in the long dataset
-interven_df <- read.csv("survey_data_long.csv")
+interven_dfL_clean <- read.csv("survey_data_long.csv")
 
 
 ######################## Table and Plot of Updates ###########################
 
-#Clean table of just updates
-interven_df_clean <- (interven_df
-                      %>% filter(interven_df, up == "Update",
-                                 (up_specific == "Update" | is.na(up_specific)),
-                                 complete == "Complete")
+
+## ROUGH VERSION ##
+
+#Clean table of all updates
+interven_df_table <- (interven_dfL_clean
+                      %>% filter(complete == "Complete")
                       %>% select(record_id, entry_time = geography_and_intro_timestamp,
                                  national_entry, country, admin1 = adm1,
                                  locality = adm_lowest, intervention, intervention_specific,
-                                 up, up_specific, status, subpopulation = pop,
-                                 date_updated = t, required, enforcement, details)
+                                 status, subpopulation = pop, date_of_update = t,
+                                 required, enforcement, details)
+                      %>% mutate(status = ifelse(status %in% c("closed", "fully closed",
+                                                               "fully restricted", "all",
+                                                               "required", "yes"), 3,
+                                          ifelse(status %in% c("partially closed", "partially restricted",
+                                                               "recommended", "some"), 2,
+                                          ifelse(status %in% c("open", "no", "no policy"), 1, status))),
+                                 status = factor(status, levels = c(1, 2, 3),
+                                                 labels = c("open/no/no policy",
+                                                            "partially closed/partially restricted/\nrecommended/some",
+                                                            "closed/restricted/all/yes")))
 )
 
-#Plot of updates
-ggplot(data = interven_df_clean %>% filter(admin1 == "USA.1_1" |
-                                             (national_entry == "Yes" &
-                                                country == "United States of America")),
-       aes(x = date_updated, y = intervention_specific,
+#Plot of all updates
+ggplot(data = interven_df_table,
+       aes(x = date_of_update, y = intervention_specific,
            shape = status, color = national_entry)) +
   geom_point(size = 3)
 
-#Need to figure out how to have country information apply to states
+
+## CREATE FUNCTION TO MAKE TABLE AND PLOT BY ADMIN1 UNIT ##
 
 
