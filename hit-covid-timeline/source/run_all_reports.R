85fcd8e7ebc8fb0c9d2467a47c8da6a416dd3569
andrew <andrewazman@gmail.com>
2020-04-08 00:47:44 +0200

    makes dirs for each country

--- hit-covid-timeline/source/run_all_reports.R
+++ hit-covid-timeline/source/run_all_reports.R
@@ -5,12 +5,12 @@ source("source/utils.R")
 source("source/data_report_functions.R")
 reload_source()
 interven_names <- read_csv("intervention_lookup.csv")
-long_data <- get_long_data(fresh_pull = FALSE, long_file_path = "generated_data/survey_data_long.csv", remove_names=TRUE)
+long_data <- get_long_data(fresh_pull = TRUE, long_file_path = "generated_data/survey_data_long.csv", remove_names=TRUE)
 last_updated_time <- file.info("generated_data/survey_data_long.csv")$mtime
 unique_locs <- long_data %>% select(country,adm1) %>% distinct
 
 date <- Sys.Date()
-out_dir <- paste0("../audit_reports/",date,"/")
+out_dir <- paste0("audit_reports/",date,"/")
 if(!dir.exists(out_dir)){
   dir.create(out_dir)
 }
@@ -18,14 +18,23 @@ if(!dir.exists(out_dir)){
 ## loop through and make reports
 ## still need to deal with a few errors
 for (i in 1:nrow(unique_locs)){
+  
   cat(sprintf("making report for %s (%s) \n",unique_locs[i,2],unique_locs[i,1]))
+  
   tryCatch({
-    out_file_name <- paste0(out_dir,paste(unique_locs[i,],collapse="-"),"_",date,".html")
+    
+    cntry_folder <- paste0(out_dir,unique_locs$country[i])
+    
+    if(!dir.exists(cntry_folder)){
+      dir.create(cntry_folder)
+    }
+    
+    out_file_name <- paste0(cntry_folder,"/",paste(unique_locs[i,],collapse="-"),"_",date,".html")
     tmp_params = list(national_input=ifelse(is.na(unique_locs[i,2]),TRUE,FALSE),
                       country_input =ifelse(is.na(unique_locs[i,2]),unique_locs[i,1],""),
                       admin_input = ifelse(is.na(unique_locs[i,2]),"",unique_locs[i,2]))
     rmarkdown::render("source/data_report.Rmd",
-                      output_file = out_file_name,
+                      output_file = paste0("../",out_file_name),
                       params = tmp_params,
                       quiet = TRUE)  
   }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
